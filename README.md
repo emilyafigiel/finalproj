8b345257f6e6778fedc85b8e5abfea082d8af551
emilyafigiel <emilyafigiel@gmail.com>
2017-05-23 22:16:00 -0400

    Add files via upload
    final-project-draft for emily

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# finalproj
+Emily Figiel
+Final Project
+
+Quiz composed of a landing page, 3 questions and then a results page all in the form of a slide navigation.  
+
+I wrote out the possible ways I could maybe use jquery but after class today, I realized it may be useful to use bootstrap carousel.js 
---
+++ final.js
@@ -0,0 +1,32 @@
+
+// homepage: have the "lets get started" button re-direct user to the next page
+//PAGE SLIDES DOWN//
+
+// $("buttonGo").click(function(){
+//     $("ul").slideDown();
+// });
+
+// First page: have the bulleted answer options be interactive so that user can click on one answer.
+//PAGE SLIDES DOWN//
+
+// $("button1").click(funtion(){
+// 	$("ul").slideDown();
+// });
+
+// Second page: same as first page
+//PAGE SLIDES DOWN//
+
+// $("button2").click(funtion(){
+// 	$("ul").slideDown();
+// });
+
+// Third page: have 3 picture thumbnails and have user be able to click on one 
+//PAGE SLIDES DOWN//
+
+// $("button3").click(funtion(){
+// 	$("ul").slideDown();
+// });
+
+// Results page: Have name of restaurant as header  
+//Drop down details of reasoning of choice on the left in a side nav bar(?) 
+//Then have the restaurant picture on right with directions
